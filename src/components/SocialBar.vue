ac62fe505fb35ffb8300ae984ba0f586c9f1d630
Roman KC <roman@bajratechnologies.com>
2021-06-20 21:43:06 +0545

    feat: update the social links component

--- src/components/SocialBar.vue
+++ src/components/SocialBar.vue
@@ -2,22 +2,22 @@
   <div class="social-wrap">
     <ul>
       <li>
-        <a :href="links.metadata.facebook" target="_blank">
+        <a :href="links.facebook" target="_blank">
           <font-awesome-icon class="icon" :icon="['fab', 'facebook-square']" />
         </a>
       </li>
       <li>
-        <a :href="links.metadata.instagram" target="_blank">
+        <a :href="links.instagram" target="_blank">
           <font-awesome-icon class="icon" :icon="['fab', 'instagram-square']" />
         </a>
       </li>
       <li>
-        <a :href="links.metadata.linkedin" target="_blank">
+        <a :href="links.linkedin" target="_blank">
           <font-awesome-icon class="icon" :icon="['fab', 'linkedin']" />
         </a>
       </li>
       <li>
-        <a :href="links.metadata.github" target="_blank">
+        <a :href="links.github" target="_blank">
           <font-awesome-icon class="icon" :icon="['fab', 'github-square']" />
         </a>
       </li>
@@ -26,21 +26,21 @@
 </template>
 
 <script>
-    export default {
-        name: 'SocialBar',
-        props: ['links'],
-    }
+export default {
+  name: "SocialBar",
+  props: ["links"],
+};
 </script>
 
 <style scoped lang="scss">
-  @import '@/styles/constants.scss';
- .social-wrap {
-    li {
-      display: inline-block;
-      margin-right: 10px;
-    }
-    .icon {
-      font-size: 3rem;
-    }
+@import "@/styles/constants.scss";
+.social-wrap {
+  li {
+    display: inline-block;
+    margin-right: 10px;
   }
+  .icon {
+    font-size: 3rem;
+  }
+}
 </style>
